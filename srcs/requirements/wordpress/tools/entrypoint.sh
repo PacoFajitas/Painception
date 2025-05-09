d4594997206d64d4b1f70654b7634105861e6bcd
root <root@Inception.myguest.virtualbox.org>
2025-05-09 21:58:34 +0200

    Jijiji

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -20,23 +20,23 @@ if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
         --dbname=${MYSQL_DATABASE} \
         --dbuser=${MYSQL_USER} \
         --dbpass=${MYSQL_PASSWORD} \
-        --dbhost=mariadb \
+        --dbhost=${MYSQL_HOSTNAME} \
         --path=/var/www/html
 
     echo "Installing WordPress..."
     wp core install --allow-root \
         --url="https://${DOMAIN_NAME}" \
         --title="${WP_TITLE}" \
-        --admin_user="${WP_ADMIN_USER}" \
-        --admin_password="${WP_ADMIN_PASSWORD}" \
-        --admin_email="${WP_ADMIN_EMAIL}"
+        --admin_user="${WP_ADMIN}" \
+        --admin_password="${WP_ADMIN_PASS}" \
+        --admin_email="${WP_ADMIN_EMAIL}" || echo "ERROOOOR"
 
     echo "Creating additional user..."
     wp user create --allow-root \
         ${WP_USER} \
-        ${WP_USER_EMAIL} \
+        ${WP_EMAIL} \
         --role=author \
-        --user_pass=${WP_USER_PASSWORD}
+        --user_pass=${WP_USER_PASS}
 
     echo "Additional configuration..."
     wp option update blogdescription "My first WordPress site using Docker" --allow-root
